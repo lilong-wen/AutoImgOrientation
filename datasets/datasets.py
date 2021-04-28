4ee4565b50f066b51ebddc4efafba03933c1ac62
wenlilong <zndxwll@gmail.com>
2021-04-28 10:22:14 +0800

    add offline image class

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -37,3 +37,28 @@ class CameraData(Dataset):
             img_rotated = self.transform(img_rotated)
 
         return img_rotated, angle_random/360
+
+
+class CameraData_offline(Dataset):
+
+    def __init__(self, root, transform=None):
+
+        self.root = root
+        self.img_list = glob.glob(root + "*.png")
+        random.shuffle(self.img_list)
+        self.transform = transform
+
+    def __len__(self):
+
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+
+        angle_random = self.img_list[idx].split("_")[-1].split(".")[0]
+
+        img = cv2.imread(self.img_list[idx])
+
+        if self.transform is not None:
+            img_rotated = self.transform(img_rotated)
+
+        return img_rotated, angle_random/360
