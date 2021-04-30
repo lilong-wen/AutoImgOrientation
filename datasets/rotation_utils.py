950d0dabdb4899f4ad71bdf83a42dbe1e47f0a70
wenlilong <zndxwll@gmail.com>
2021-04-30 11:19:19 +0800

    correct angle

--- datasets/rotation_utils.py
+++ datasets/rotation_utils.py
@@ -1,4 +1,4 @@
-import cv2
+nimport cv2
 import numpy as np
 import math
 import random
@@ -36,7 +36,7 @@ def rotate(image, angle, center = None, scale = 1.0):
     if center is None:
         center = (w / 2, h / 2)
 
-    M = cv2.getRotationMatrix2D(center, angle, scale)
+    M = cv2.getRotationMatrix2D(center, -angle, scale)
     rotated = cv2.warpAffine(image, M, (w, h), borderMode=cv2.BORDER_WRAP)
 
     return rotated
